f597b596be0d9a47aea315ce77c014cc9686c6aa
Wesley Mascarenhas <hebeu.pipo@gmail.com>
2018-08-25 14:06:51 -0300

    .

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -26,7 +26,7 @@ public class Controller extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("controller.Controller.main()");
+        System.out.println("Teste2");
         launch(args);
     }
     
