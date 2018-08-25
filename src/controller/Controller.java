b86497e013bb2272d248f5c3e47c69e678f0049b
Wesley Mascarenhas <hebeu.pipo@gmail.com>
2018-08-25 11:24:29 -0300

    Usando JavaFX

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,18 +6,49 @@
 
 package controller;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 /**
  *
  * @author wesle
  */
-public class Controller {
+public class Controller extends Application {
+    
+    private static final int JFXPANEL_WIDTH_INT = 300;
+    private static final int JFXPANEL_HEIGHT_INT = 250;
+    private static JFXPanel fxContainer;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("ois");
+        launch(args);
     }
     
+   
+        @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLconV.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 }
