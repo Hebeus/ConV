8f396b0ca2cce8325232f78acc74e75c0a442777
Wesley Mascarenhas <hebeu.pipo@gmail.com>
2018-08-25 13:58:38 -0300

    .

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,24 +6,12 @@
 
 package controller;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javax.swing.JApplet;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
 /**
  *
  * @author wesle
