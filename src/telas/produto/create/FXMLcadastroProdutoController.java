1b725c6dfcaa684c4068ed7fad5cfd450982cf50
Wesley Mascarenhas <hebeu.pipo@gmail.com>
2018-08-27 00:37:31 -0300

    Criando telas das classes

---
+++ src/telas/produto/create/FXMLcadastroProdutoController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telas.produto.create;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import model.CategoriaProduto;
+import model.Produto;
+
+/**
+ * FXML Controller class
+ *
+ * @author wesle
+ */
+public class FXMLcadastroProdutoController implements Initializable {
+    private ArrayList<CategoriaProduto> cartegoriaGeral;
+    private Produto produto;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
